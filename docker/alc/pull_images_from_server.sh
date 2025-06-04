c97e04e71e06f64edcd5df5da1fe0d3d19666f03
nag <nag.mahadevan@gmail.com>
2025-06-04 10:27:35 -0500

    updated images

--- docker/alc/pull_images_from_server.sh
+++ docker/alc/pull_images_from_server.sh
@@ -2,9 +2,7 @@
 
 server_name="ablecps"
 
-image_list=("alc:latest"
-            "alc_dind:latest"
-            "xpra_16.04:latest"
+image_list=("xpra_16.04:latest"
             "alc_data:latest"
             )
 
@@ -21,8 +19,26 @@ for (( i=0; i<${#image_list[*]}; ++i)); do
     docker tag $image_tagged_name $image_name
 done
 
-image_list=("registry:2"
-            "jkarlos/git-server-docker:latest"
+
+server_name="git.isis.vanderbilt.edu:5050/alcdockers/alc"
+
+image_list=("alc:cpu"
+            )
+
+for (( i=0; i<${#image_list[*]}; ++i)); do
+    image_name=${image_list[$i]}
+    image_tagged_name=${server_name}/$image_name
+    echo 'pulling image from server - '$image_tagged_name
+    docker pull $image_tagged_name
+    if [[ "$(docker images -q $image_name 2> /dev/null)" != "" ]]; then
+        echo 'removing old image - '$image_name
+        docker rmi $image_name
+    fi
+    echo 'tagging image from server for use - '$image_name
+    docker tag $image_tagged_name $image_name
+done
+
+image_list=("jkarlos/git-server-docker:latest"
             "erichough/nfs-server:latest"
             "mongo:3.4.6"
             "nginx:stable"
